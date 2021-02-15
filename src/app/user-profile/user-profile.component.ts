2e4498db73e6a65d8af4dffe8f7cfc45519dc35c
DIERICK Dewi <dierickd@outlook.fr>
2021-02-15 11:29:50 +0100

    create user-profile component and custom style

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+  constructor() {
+  }
+
+  User = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  toggle = false;
+
+  toggleAge(): void {
+    this.toggle = !this.toggle;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
