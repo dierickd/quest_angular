aff736579fd6d7e0933cdac66194a95f66f500a7
DIERICK Dewi <dierickd@outlook.fr>
2021-02-19 08:35:32 +0100

    quest3: show button if isAdmin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { UserProfileComponent } from './user-profile/user-profile.component'; // <-- NgModel lives here
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { IsAdminDirective } from './is-admin.directive'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    IsAdminDirective
   ],
   imports: [
     BrowserModule,
