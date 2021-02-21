f42dc3012559eca28813b9e7a9d26c80950d9200
DIERICK Dewi <dierickd@outlook.fr>
2021-02-21 09:30:01 +0100

    quest4 create route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { UserProfileComponent } from './user-profile/user-profile.component'; // <-- NgModel lives here
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HeaderComponent } from './header/header.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    HeaderComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
